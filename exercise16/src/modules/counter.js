98967095941be3299b247d6d29546a7392b0e32e
gayoung-koh <gayoung.koh@hanwha-qcells.com>
2020-07-29 09:26:53 +0900

    studied opensource based redux-middleware(redux-thunk, redux-saga) for processing async

--- exercise16/src/modules/counter.js
+++ exercise16/src/modules/counter.js
@@ -1,10 +1,20 @@
 import { createAction, handleActions } from "redux-actions";
-import { delay, put, takeEvery, takeLatest } from "redux-saga/effects";
+import {
+  delay,
+  put,
+  takeEvery,
+  takeLatest,
+  select,
+  throttle,
+} from "redux-saga/effects";
 
 const INCREASE = "counter/INCREASE";
 const DECREASE = "counter/DECREASE";
 const INCREASE_ASYNC = "counter/INCREASE_ASYNC";
 const DECREASE_ASYNC = "counter/DECREASE_ASYNC";
+
+export const increase = createAction(INCREASE);
+export const decrease = createAction(DECREASE);
 // 마우스 클릭 이벤트가 payload 안에 들어가지 않도록
 // () => undefined를 두 번째 파라미터로 넣어 줍니다.
 export const increaseAsync = createAction(INCREASE_ASYNC, () => undefined);
@@ -13,6 +23,8 @@ export const decreaseAsync = createAction(DECREASE_ASYNC, () => undefined);
 function* increaseSaga() {
   yield delay(1000);
   yield put(increase());
+  const number = yield select((state) => state.counter);
+  console.log(`현재 값은 ${number}입니다.`);
 }
 
 function* decreaseSaga() {
@@ -22,7 +34,7 @@ function* decreaseSaga() {
 
 export function* counterSaga() {
   // takeEvery는 들어오는 모든 액션에 대해 특정 작업을 처리해 줍니다.
-  yield takeEvery(INCREASE_ASYNC, increaseSaga);
+  yield throttle(3000, INCREASE_ASYNC, increaseSaga);
   // takeLatest는 기존에 진행 중이던 작업이 있다면 취소 처리하고
   // 가장 마지막으로 실행된 작업만 수행합니다.
   yield takeLatest(DECREASE_ASYNC, decreaseSaga);
